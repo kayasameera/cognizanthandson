c3f2ec2106b93144adbce6686985690ad0ef41fe
kayasameera <204695240+kayasameera@users.noreply.github.com>
2026-07-10 14:22:51 +0530

    Add files via upload

---
+++ plsql/create_tables.sql
@@ -0,0 +1,16 @@
+CREATE TABLE Customers (
+    CustomerID NUMBER PRIMARY KEY,
+    CustomerName VARCHAR2(50),
+    Age NUMBER,
+    Balance NUMBER,
+    IsVIP VARCHAR2(10)
+);
+
+CREATE TABLE Loans (
+    LoanID NUMBER PRIMARY KEY,
+    CustomerID NUMBER,
+    InterestRate NUMBER,
+    DueDate DATE,
+    FOREIGN KEY (CustomerID)
+    REFERENCES Customers(CustomerID)
+);
